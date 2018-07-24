d74c8b711f2155bc8dd86ab90ee02f4a5a0f210e
jim <535258211@qq.com>
2018-07-24 13:27:59 +0800

    webpack init

---
+++ webpack/src/js/index.js
@@ -0,0 +1,8 @@
+// 1. 导入样式表
+import '../css/index.css'
+
+// 2. 奇数行一个颜色 :odd
+$('#ulList>li:odd').css('backgroundColor','lightblue');
+
+// 3. 偶数行一个颜色 :even
+$('#ulList>li:even').css('backgroundColor','blue');
